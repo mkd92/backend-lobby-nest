bb177fed5d1b502725f66e95d64714b4014349bf
mkd92 <manikandan.m1116@gmail.com>
2022-11-17 20:03:29 +0530

    cors added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { JwtGuard } from 'src/auth/guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: 'http://localhost:5173',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
